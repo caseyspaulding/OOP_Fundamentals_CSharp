6effe8175ac220f2d4e96872b0c39a2c2d10305a
Casey Spaulding <casey.spaulding@me.com>
2023-04-29 08:01:43 -0400

    blazor practice

---
+++ BlazorAppPRACTICE/BlazorAppPRACTICE/Pages/CounterBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorAppPRACTICE.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+
+        protected int currentCount = 0;
+
+        protected void IncrementCount()
+        {
+            currentCount += 2;
+        }
+
+    }
+}
